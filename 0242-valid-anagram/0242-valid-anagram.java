8a30d0a45119511e742bd9b0d88982929f78da87
vaishnavi devi <se.vaishnavidevi07@gmail.com>
2026-09-17 09:07:20 +0530

    Time: 24 ms (5.16%), Space: 46 MB (49.9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer>map1 = new HashMap<>();
+        HashMap<Character,Integer>map2= new HashMap<>();
+
+        if (s.length() != t.length()){
+            return false;
+        }
+        for (int i =0;i<s.length();i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            map1.put(ch1, map1.getOrDefault(ch1,0)+1);
+            map2.put(ch2,map2.getOrDefault(ch2,0)+1);
+        }
+        return map1.equals(map2);
+
+    }
+}
