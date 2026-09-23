9f3c829740622de8d609e05f1a37f94d76d1a6b4
vaishnavi devi <se.vaishnavidevi07@gmail.com>
2026-09-23 09:21:01 +0530

    Time: 3 ms (50.80%) | Memory: 45.7 MB (99.17%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[]count=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            count[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            int index = ransomNote.charAt(i)-'a';
+            count[index]--;
+            if(count[index]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
